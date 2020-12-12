92a38f966f7dd95d95316bce5124f1c0ab2b9236
rogtom <tomasz.rogala1505@gmail.com>
2020-12-12 17:24:59 +0100

    statistic 4 steps section done

---
+++ src/components/layout/home/oCoChodziSection/4steps.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { ReactComponent as Decoration } from '../../../../assets/Decoration.svg'
+
+const ForSteps = () => {
+    return (
+        <div className="for-steps__wrapper">
+            <h2 className="for-steps__title">wystarczą 4 proste kroki</h2>
+            <Decoration className="for-steps__decoration"/>
+        </div>
+    )
+}
+export default ForSteps
