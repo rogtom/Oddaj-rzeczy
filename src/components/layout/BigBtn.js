c8158ff344629d8ef9a6c06e6b9f275a4bbfdde3
rogtom <tomasz.rogala1505@gmail.com>
2020-12-12 22:20:00 +0100

    buttons update

--- src/components/layout/BigBtn.js
+++ src/components/layout/BigBtn.js
@@ -1,4 +1,4 @@
 import React from "react";
 
-const BigBtn = ({text1, text2}) => <div className="big-btn main-btn__give"><span>{text1}</span><span>{text2}</span></div>
+const BigBtn = ({text1, text2}) => <div className="big-btn"><span>{text1}</span><span>{text2}</span></div>
 export default BigBtn
--- src/components/layout/home/startSection/MainBtns.js
+++ src/components/layout/home/startSection/MainBtns.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BigBtn from "../../BigBtn";
+import { Link } from 'react-router-dom'
+import * as ROUTES from '../../../../constants/routes';
 
 const MainButtons = () => {
     return (
         <>
             {/*<div className="main-btn__give"><span>oddaj</span><span>rzeczy</span></div>*/}
-            <BigBtn text1={'oddaj'} text2={'rzeczy'}/>
-            <BigBtn text1={'zorganizuj'} text2={'zbiórkę'}/>
+            <Link to={ROUTES.SIGN_IN}><BigBtn text1={'oddaj'} text2={'rzeczy'} /></Link>
+            <Link to={ROUTES.SIGN_IN}><BigBtn text1={'zorganizuj'} text2={'zbiórkę'} /></Link>
             {/*<div className="main-btn__organize"><span>zorganizuj</span><span>zbiórkę</span></div>*/}
         </>
     )
