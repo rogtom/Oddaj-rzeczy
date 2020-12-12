93cbd2c96ad9417984e6162332433f5a5d9af607
rogtom <tomasz.rogala1505@gmail.com>
2020-12-12 17:26:38 +0100

    statistic give away section done

---
+++ src/components/layout/home/oCoChodziSection/WhatsUp.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Statistics from "./Statistic";
+import ForSteps from "./4steps";
+import WhatToDo from "./whatToDo";
+import GiveAway from "./giveAway";
+
+
+
+
+const WhatsUp = () => {
+    return (
+
+        <>
+            <Statistics/>
+            <ForSteps/>
+            <WhatToDo/>
+            <GiveAway/>
+        </>
+    )
+}
+export default WhatsUp
---
+++ src/components/layout/home/oCoChodziSection/giveAway.js
@@ -0,0 +1,12 @@
+import React from "react";
+import BigBtn from "../../BigBtn";
+
+
+const GiveAway = () => {
+    return (
+        <div className="give-away__wrapper">
+            <BigBtn text1={'oddaj'} text2={'rzeczy'}/>
+        </div>
+    )
+}
+export default GiveAway
