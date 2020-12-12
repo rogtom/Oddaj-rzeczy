c078aefd6dcf00890d8af0338bc318aa7f7c43dd
rogtom <tomasz.rogala1505@gmail.com>
2020-12-12 22:18:31 +0100

    signup form added

--- src/components/layout/SignUp.js
+++ src/components/layout/SignUp.js
@@ -1,9 +1,25 @@
-import React from "react";
+import {ReactComponent as Decoration} from '../../assets/Decoration.svg'
+import {Link} from "react-router-dom";
+import * as ROUTES from "../../constants/routes";
 
 const SignUp = () => {
     return (
-        <h1>Sign Up</h1>
+        <div className="sign-in__wrapper">
+            <h1 className="sign-in__title">Załóż konto</h1>
+            <Decoration className="sign-in__decoration"/>
+            <form className="sign-in__form">
+                <label htmlFor="">Email</label>
+                <input type="email" name={'email'}/>
+                <label htmlFor="">Hasło</label>
+                <input type="password" name={'password'}/>
+                <label htmlFor="">Powtórz hasło</label>
+                <input type="password" name={'password'}/>
+            </form>
+            <div className="sign-in__form__btn-wrapper">
+                <Link to={ROUTES.SIGN_IN}>Zaloguj się</Link>
+                <button type={"submit"}>Załóż konto</button>
+            </div>
+        </div>
     )
 }
-
-export default SignUp
+export default SignUp;
