d18c60314736296d1c0417d906172bda9b3d1ea5
rogtom <tomasz.rogala1505@gmail.com>
2020-12-12 17:27:22 +0100

    updates in files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route,} from "react-router-dom";
+import * as ROUTES from './constants/routes';
+import Home from "./components/layout/home/Home";
+import Header from "./components/layout/Header";
+import SignIn from "./components/layout/SignIn";
+import SignUp from "./components/layout/SignUp";
+
 
 const App = () => {
     return (
-        <h1>oddaj</h1>
+        <Router>
+
+            <Header/>
+
+            <Switch>
+                <Route exact path={ROUTES.HOME} component={Home} />
+                <Route path={ROUTES.SIGN_IN} component={SignIn}/>
+                <Route path={ROUTES.SIGN_UP} component={SignUp}/>
+
+            </Switch>
+
+        </Router>
     )
 }
 
---
+++ src/components/layout/home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Main from "./startSection/Main";
+import WhatsUp from "./oCoChodziSection/WhatsUp";
+
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Main/>
+            <WhatsUp />
+
+        </div>
+    )
+}
+
+export default Home
