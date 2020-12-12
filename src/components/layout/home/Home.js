771726ff8b6a8ab79bf7e04e387004f39cf122fd
rogtom <tomasz.rogala1505@gmail.com>
2020-12-12 19:50:25 +0100

    scroll to top add

--- src/components/layout/home/Home.js
+++ src/components/layout/home/Home.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Main from "./startSection/Main";
 import WhatsUp from "./oCoChodziSection/WhatsUp";
+import AboutUs from "./AboutUs";
+import ScrollToTop from "./ScrollToTop";
+import { animateScroll as scroll } from 'react-scroll'
+
 
 
 
 const Home = () => {
     return (
         <div className="home">
-            <Main/>
+            <Main />
             <WhatsUp />
+            <AboutUs/>
 
+
+            <ScrollToTop/>
         </div>
     )
 }
